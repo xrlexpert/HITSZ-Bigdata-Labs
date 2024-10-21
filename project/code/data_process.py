b4b921a46feb7d40dff97ec476cc7be10780ea11
hirox <hiroxrl@outlook.com>
2024-10-21 20:49:42 +0800

    fix bug

--- project/code/data_process.py
+++ project/code/data_process.py
@@ -11,14 +11,26 @@ test_id = test['Id']
 train.drop(['Id'], axis=1, inplace=True)
 
 continuous_columns = train.select_dtypes(include=['int64', 'float64']).columns.tolist()
+continuous_columns.remove('SalePrice')
+# need_to_remove = ['MSSubClass','MoSold', 'YrSold','GarageYrBlt','SalePrice']
+# for col in need_to_remove:
+#     continuous_columns.remove(col)
+
 discret_columns = train.select_dtypes(include=['object']).columns.tolist()
+# need_to_add = ['MSSubClass','MoSold', 'YrSold','GarageYrBlt']
+# for col in need_to_add:
+#     discret_columns.append(col)
+
+for col in continuous_columns:
+    train[col] = train[col].fillna(train[col].median())
+    test[col] = test[col].fillna(test[col].median())
 
 for col in discret_columns:
-    train[col].fillna(-1,inplace=True)
-    test[col].fillna(-1,inplace=True)
+    train[col].fillna('-1',inplace=True)
+    test[col].fillna('-1',inplace=True)
     res1 = train[col].value_counts().keys()
     res2 = test[col].value_counts().keys()
-    res = list(set(res1).union(set(res2)))
+    res = sorted(list(set(res1).union(set(res2))))
     mapping = dict(zip(res, range(len(res))))
     print(f'{col}:{mapping}')
     train[col] = train[col].map(mapping)
